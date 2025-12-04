3c4ae68f4aa4149cabb68cbc5bb13b54ba211340
sodervan <egbowonemmanuel99@gmail.com>
2025-12-04 11:42:09 +0100

    removed the former codebase

--- NeededHome_Web-main/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@tanstack/react-router";
-
-export const Loader = () => {
-  console.log("Loader executed");
-  return redirect({ to: "/home" });
-};
-export default function index() {
-  return <div></div>;
-}
