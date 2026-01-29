977ac1b9d8ac032a2bc568b63ee57d15d5d6d6e8
sodervan <egbowonemmanuel99@gmail.com>
2026-01-29 08:51:23 +0100

    done

--- src/routes/about-us.tsx
+++ src/routes/about-us.tsx
@@ -147,7 +147,7 @@ function RouteComponent() {
               <div className="relative flex justify-center">
                 {/* Using a structural/architectural image */}
                 <img
-                  src="https://images.unsplash.com/photo-1518005020411-38b83d23c91d?auto=format&fit=crop&q=80&w=800"
+                  src="https://images.unsplash.com/photo-1515263487990-61b07816b324?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="Architectural Archway"
                   className="w-full max-w-md grayscale opacity-80"
                 />
