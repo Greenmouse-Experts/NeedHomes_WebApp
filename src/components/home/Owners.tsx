b9110ecf10820ab50ecfbf353298561fb621571c
sodervan <egbowonemmanuel99@gmail.com>
2026-01-30 20:17:52 +0100

    done

--- src/components/home/Owners.tsx
+++ src/components/home/Owners.tsx
@@ -1,10 +1,10 @@
-import { Button } from '../ui/Button'
-import FlexInfo from './FlexInfo'
-import Sectiongrid from './SectionGrid'
-import { useNavigate } from '@tanstack/react-router'
+import { Button } from "../ui/Button";
+import FlexInfo from "./FlexInfo";
+import Sectiongrid from "./SectionGrid";
+import { useNavigate } from "@tanstack/react-router";
 
 export default function Owners() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="py-12">
       <Sectiongrid>
@@ -19,16 +19,22 @@ export default function Owners() {
             </h2>
             <p className="text-lg">
               Fractional ownership is a structure in which multiple individuals
-              or entities can each purchase a portion of an asset. Each investor buys a fraction of the property, sharing both
-              the cost and the potential profit.
+              or entities can each purchase a portion of an asset. Each investor
+              buys a fraction of the property, sharing both the cost and the
+              potential profit.
             </p>
-            <Button variant="outline" size="lg" onClick={() => { navigate({ to: '/how-it-works' }) }}>
+            <Button
+              variant="outline"
+              size="lg"
+              onClick={() => {
+                navigate({ to: "/fractional-ownership" });
+              }}
+            >
               Read more
             </Button>
           </div>
         </FlexInfo>
       </Sectiongrid>
     </div>
-  )
+  );
 }
-
