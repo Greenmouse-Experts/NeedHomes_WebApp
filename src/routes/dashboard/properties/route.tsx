94358ef965e58ae11c83fd5ef659001e713941d5
destiny <desto4q@gmail.com>
2026-02-04 20:00:16 +0100

    subscriptions user

--- src/routes/dashboard/properties/route.tsx
+++ src/routes/dashboard/properties/route.tsx
@@ -1,4 +1,5 @@
 import { DashboardLayout } from "@/components/dashboard/DashboardLayout";
+import ThemeProvider from "@/simpleComps/ThemeProvider";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/properties")({
@@ -9,7 +10,9 @@ function RouteComponent() {
   return (
     <>
       <DashboardLayout subtitle="Propert Listing" title="SuperAdmin Dashboard">
-        <Outlet />
+        <ThemeProvider>
+          <Outlet />
+        </ThemeProvider>
       </DashboardLayout>
     </>
   );
