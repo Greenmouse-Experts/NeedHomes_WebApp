97f37b6b43e65391ba7569a94a074718c7d509a3
destiny <desto4q@gmail.com>
2026-02-04 17:47:59 +0100

    updated onwders; fractional ;land-banking

--- src/routes/investors/subscriptions/index.tsx
+++ src/routes/investors/subscriptions/index.tsx
@@ -1,4 +1,6 @@
-import apiClient from "@/api/simpleApi";
+import apiClient, { type ApiResponse } from "@/api/simpleApi";
+import PageLoader from "@/components/layout/PageLoader";
+import ThemeProvider from "@/simpleComps/ThemeProvider";
 import { useAuth } from "@/store/authStore";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
@@ -10,8 +12,8 @@ export const Route = createFileRoute("/investors/subscriptions/")({
 function RouteComponent() {
   const [auth] = useAuth();
   const user = auth?.user;
-  const query = useQuery({
-    queryKey: ["subscriptions", user],
+  const query = useQuery<ApiResponse<[]>>({
+    queryKey: ["subscriptions-plans", user],
     queryFn: async () => {
       let resp = await apiClient.get("/subscriptions/plans");
       return resp.data;
@@ -21,6 +23,11 @@ function RouteComponent() {
   return (
     <>
       <CurrentPlan />
+      <PageLoader query={query}>
+        {(data) => {
+          return <>{JSON.stringify(data)}</>;
+        }}
+      </PageLoader>
     </>
   );
 }
@@ -28,12 +35,30 @@ function RouteComponent() {
 const CurrentPlan = () => {
   const [auth] = useAuth();
   const user = auth?.user;
-  const query = useQuery({
+  const { data, isLoading, error } = useQuery<ApiResponse<any>>({
     queryKey: ["subscriptions", user],
     queryFn: async () => {
       let resp = await apiClient.get("subscriptions/my-subscription");
       return resp.data;
     },
   });
-  return <div>current plan</div>;
+
+  return (
+    <ThemeProvider>
+      <div className="card ring fade p-4 shadow-lg rounded-lg">
+        {error && <div className="alert alert-danger">Error fetching plan</div>}
+        {isLoading && <div className="spinner"></div>}
+        {!data || data.length === 0 ? (
+          <div className="card card-primary">
+            <h2 className="heading heading-primary">Free Plan</h2>
+            <p className="text">This is the free subscription plan.</p>
+          </div>
+        ) : (
+          <div className="card card-success">
+            <h2 className="heading heading-success">Current Plan</h2>
+          </div>
+        )}
+      </div>
+    </ThemeProvider>
+  );
 };
