ade9627459b0ac46457fc2844af0ef6bd21a457c
destiny <desto4q@gmail.com>
2026-02-05 21:57:21 +0100

    kyc

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -20,7 +20,7 @@ export const user_atom = atomWithStorage<AUTHRECORD | null>(
 );
 export const kyc_atom = atomWithStorage<USER_KYC | null>(
   "kyc",
-  stored ? JSON.parse(userKyc) : null,
+  userKyc ? JSON.parse(userKyc) : null,
 );
 export const useKyc = () => {
   const [kyc, setKyc] = useAtom(kyc_atom);
