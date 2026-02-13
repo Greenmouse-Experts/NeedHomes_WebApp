1a015bf1cb91d60f0860daa72b3d965c906c8e20
destiny <desto4q@gmail.com>
2026-02-13 19:21:25 +0100

    promotions

--- src/components/partners/PartnerSidebar.tsx
+++ src/components/partners/PartnerSidebar.tsx
@@ -50,12 +50,12 @@ const NAV_LINKS = [
     id: "announcements",
     icon: Megaphone,
   },
-  // {
-  //   to: "/investors/subscriptions",
-  //   label: "Subscriptions",
-  //   id: "subscriptions",
-  //   icon: List,
-  // },
+  {
+    to: "/investors/promotions",
+    label: "Promotions",
+    id: "promotions",
+    icon: List,
+  },
   {
     to: "/partners/settings",
     label: "Setting",
