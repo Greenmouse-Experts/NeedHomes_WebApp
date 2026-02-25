b8c55168da4972828b053e4c53c1186818298a6a
destiny <desto4q@gmail.com>
2026-02-25 09:39:47 +0100

    total price update

--- src/routes/dashboard/properties/edit/$propertyId/outright.tsx
+++ src/routes/dashboard/properties/edit/$propertyId/outright.tsx
@@ -120,10 +120,10 @@ function FormField({ defaultValue }: { defaultValue: PROPERTY_TYPE }) {
       // Handle Video Upload
       let videoUrl = await video_helper(videoUpload);
       const totalPrice =
-        Number(data.basePrice) +
+        Number(data.basePrice * 100) +
         (data.additionalFees
           ? data.additionalFees.reduce(
-              (acc, fee) => acc + (Number(fee.amount) || 0),
+              (acc, fee) => acc + (Number(fee.amount * 100) || 0),
               0,
             )
           : 0);
