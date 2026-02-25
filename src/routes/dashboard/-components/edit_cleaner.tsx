b4de4795b9fa192f1c9cf98038cd225a4adc4c48
destiny <desto4q@gmail.com>
2026-02-25 13:10:40 +0100

    price corrections

--- src/routes/dashboard/-components/edit_cleaner.tsx
+++ src/routes/dashboard/-components/edit_cleaner.tsx
@@ -1,6 +1,9 @@
 import type { ADMIN_PROPERTY_LISTING } from "@/types";
 
-export default function edit_cleaner(data: ADMIN_PROPERTY_LISTING) {
+export default function edit_cleaner(
+  data: ADMIN_PROPERTY_LISTING,
+  extraClean: string[],
+) {
   const new_data = {
     ...data,
     basePrice: data.basePrice / 100,
@@ -10,5 +13,12 @@ export default function edit_cleaner(data: ADMIN_PROPERTY_LISTING) {
     })),
     totalPrice: data.totalPrice / 100,
   } as ADMIN_PROPERTY_LISTING;
+  if (extraClean.length > 0) {
+    for (const key of extraClean) {
+      if (key in new_data && typeof (new_data as any)[key] === "number") {
+        (new_data as any)[key] = (new_data as any)[key] / 100;
+      }
+    }
+  }
   return new_data;
 }
