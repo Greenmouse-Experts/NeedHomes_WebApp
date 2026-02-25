06018e779b68d2d8c16d4c00ad373dcb9878638c
destiny <desto4q@gmail.com>
2026-02-25 13:15:31 +0100

    price corrections

--- src/routes/dashboard/properties/edit/$propertyId/outright.tsx
+++ src/routes/dashboard/properties/edit/$propertyId/outright.tsx
@@ -78,6 +78,7 @@ function RouteComponent() {
           }
           return (
             <>
+              {/*//@ts-ignore*/}
               <FormField defaultValue={new_data} />
             </>
           );
