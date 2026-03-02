7918a3e13f4dd8748509d48df151e8e8c8b30ee0
destiny <desto4q@gmail.com>
2026-03-02 19:26:45 +0100

    blog details

--- src/routes/dashboard/blogs/$id/details/index.tsx
+++ src/routes/dashboard/blogs/$id/details/index.tsx
@@ -1,9 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
+import apiClient from "@/api/simpleApi";
+import PageLoader from "@/components/layout/PageLoader";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/dashboard/blogs/$id/details/')({
+export const Route = createFileRoute("/dashboard/blogs/$id/details/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/blogs/$id/details/"!</div>
+  const { id } = Route.useParams();
+  const query = useQuery({
+    queryKey: ["blog-details", id],
+    queryFn: async () => {
+      let resp = await apiClient.get("blogs/admin/" + id);
+      return resp.data;
+    },
+  });
+  return (
+    <PageLoader query={query}>
+      {(resp) => {
+        let data = resp.data;
+        return <div>{JSON.stringify(data)}</div>;
+      }}
+    </PageLoader>
+  );
 }
