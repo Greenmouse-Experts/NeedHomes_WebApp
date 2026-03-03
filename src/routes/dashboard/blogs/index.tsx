1e397110011b1f0348c315ca5511f7be425bdb93
destiny <desto4q@gmail.com>
2026-03-03 14:12:15 +0100

    blog publishing

--- src/routes/dashboard/blogs/index.tsx
+++ src/routes/dashboard/blogs/index.tsx
@@ -1,22 +1,155 @@
 import apiClient, { type ApiResponseV2 } from "@/api/simpleApi";
 import PageLoader from "@/components/layout/PageLoader";
+import CustomTable, { type columnType } from "@/components/tables/CustomTable";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
 import { createFileRoute } from "@tanstack/react-router";
-import { BlogCard } from "./-components/BlogCard";
+import { type Actions } from "@/components/tables/pop-up";
+import { useNavigate } from "@tanstack/react-router";
+import { useState } from "react";
+import SearchBar from "@/routes/-components/Searchbar";
+import { toast } from "sonner";
+import { extract_message } from "@/helpers/apihelpers";
 
 export const Route = createFileRoute("/dashboard/blogs/")({
   component: RouteComponent,
 });
 
+interface Blog {
+  id: string;
+  title: string;
+  slug?: string;
+  content: string;
+  authorId: string;
+  isPublished: boolean;
+  photoUrl: string[];
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+  allowComments: boolean;
+  author: {
+    id: string;
+    firstName: string;
+    lastName: string;
+  };
+  blogCategories: any[];
+  _count: {
+    comments: number;
+  };
+}
+
+const status = [
+  {
+    name: "All",
+    key: null,
+  },
+  {
+    name: "Published",
+    key: "PUBLISHED",
+  },
+  {
+    name: "Draft",
+    key: "DRAFT",
+  },
+];
+
 function RouteComponent() {
-  const query = useQuery<ApiResponseV2<[]>>({
-    queryKey: ["blogs-admin"],
+  const nav = useNavigate();
+  const [search, setSearch] = useState();
+  const query = useQuery<ApiResponseV2<Blog[]>>({
+    queryKey: ["blogs-admin", search],
     queryFn: async () => {
-      const response = await apiClient.get("blogs/admin/all");
+      const response = await apiClient.get("blogs/admin/all", {
+        params: {
+          search: search,
+          // status: "PUBLISHED",
+        },
+      });
       return response.data;
     },
   });
+
+  const columns: columnType<Blog>[] = [
+    {
+      key: "title",
+      label: "Title",
+      render: (value) => <span className="line-clamp-1">{value}</span>,
+    },
+    {
+      key: "author",
+      label: "Author",
+      render: (_, item) => `${item.author.firstName} ${item.author.lastName}`,
+    },
+    {
+      key: "isPublished",
+      label: "Status",
+      render: (value) => (
+        <span
+          className={`badge badge-sm badge-soft ring  fade ${value ? "badge-success" : "badge-warning"}`}
+        >
+          {value ? "Published" : "Draft"}
+        </span>
+      ),
+    },
+    {
+      key: "createdAt",
+      label: "Created At",
+      render: (value) => new Date(value).toLocaleDateString(),
+    },
+    {
+      key: "_count",
+      label: "Comments",
+      render: (_, item) => item._count.comments,
+    },
+  ];
+
+  const actions: Actions<Blog>[] = [
+    {
+      key: "edit",
+      label: "Edit",
+      action: (item) => {
+        nav({ to: `/dashboard/blogs/${item.id}/edit` });
+      },
+    },
+    {
+      key: "status",
+      label: "publish",
+      action: (item) => {
+        return toast.promise(() => publish(item.id, !item.isPublished), {
+          loading: "Publishing...",
+          success: () => {
+            query.refetch();
+            return "Published successfully!";
+          },
+          error: extract_message,
+        });
+      },
+      render: (item) => {
+        return <>{item.isPublished ? "Unpublish" : "Publish"}</>;
+      },
+    },
+    {
+      key: "view",
+      label: "View",
+      action: (item) => {
+        nav({ to: `/blogs/${item.slug || item.id}` });
+      },
+    },
+    {
+      key: "delete",
+      label: "Delete",
+      action: (item) => {
+        // Add delete logic here
+        console.log("Delete", item);
+      },
+    },
+  ];
+  const publish = async (id: string, isPublished: boolean) => {
+    let resp = await apiClient.patch(`blogs/${id}/publish`, {
+      isPublished: isPublished,
+    });
+    return resp.data;
+  };
   return (
     <>
       <section className="flex flex-col md:flex-row">
@@ -31,19 +164,19 @@ function RouteComponent() {
           Create Blog
         </Link>
       </section>
+      <div className="mb-6">
+        <SearchBar value={search} onChange={setSearch} />
+      </div>
       <PageLoader query={query}>
         {(data) => {
           const blogs = data.data.data;
           return (
-            <div className="grid grid-cols-4 gap-2">
-              {blogs.map((blog) => {
-                return (
-                  <>
-                    <BlogCard post={blog} />
-                  </>
-                );
-              })}
-            </div>
+            <CustomTable
+              data={blogs}
+              columns={columns}
+              actions={actions}
+              totalCount={blogs.length}
+            />
           );
         }}
       </PageLoader>
