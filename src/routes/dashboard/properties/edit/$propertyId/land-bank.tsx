0d457f99b1f6508701d75d8e75c7ced9c74d63ec
destiny <desto4q@gmail.com>
2026-03-05 15:57:56 +0100

    availablePlot size

--- src/routes/dashboard/properties/edit/$propertyId/land-bank.tsx
+++ src/routes/dashboard/properties/edit/$propertyId/land-bank.tsx
@@ -179,6 +179,7 @@ function FormField({ defaultValue }: { defaultValue: PROPERTY_TYPE }) {
         </div>
         <DefaultForm
           update
+          disableUnis
           disable_base_price
           docUpload={docUpload}
           videoUpload={videoUpload}
