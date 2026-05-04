39bd928184f20edbb976c9661d19002b8647328b
destiny <desto4q@gmail.com>
2026-05-04 20:21:08 +0100

    member ship

--- src/components/home/BankIntegrations.tsx
+++ src/components/home/BankIntegrations.tsx
@@ -1,14 +1,13 @@
 export default function BankIntegrations() {
   const banks = [
-    { name: 'Stanbic', logo: '/banks/stanbic.png' },
-    { name: 'Paystack', logo: '/banks/paystack.png' },
-    { name: 'Alat', logo: '/banks/alat.png' },
-  ]
+    { name: "membership_1", logo: "/membership/member_1.png" },
+    { name: "membership_2", logo: "/membership/member_2.png" },
+  ];
 
   return (
     <div className="contain mx-auto py-22 px-4 md:px-6">
       <h2 className="text-center mx-auto max-w-xl text-3xl font-semibold">
-        Integrate with leading financial institution
+        Professional Membership
       </h2>
       <div className="mt-12">
         <div className="flex gap-8 md:gap-16 justify-center items-center flex-wrap">
@@ -27,6 +26,5 @@ export default function BankIntegrations() {
         </div>
       </div>
     </div>
-  )
+  );
 }
-
